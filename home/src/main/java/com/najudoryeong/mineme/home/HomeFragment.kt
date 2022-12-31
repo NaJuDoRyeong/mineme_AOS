bbf0e1b9af16e99f36c0fc055075ba30852299d4
KDW03 <aszx0628@naver.com>
2022-12-31 19:28:56 +0900

    [MIN-24] feature : Home 모듈 생성 및 네비게이션 연결(#12)

---
+++ home/src/main/java/com/najudoryeong/mineme/home/HomeFragment.kt
@@ -0,0 +1,14 @@
+package com.najudoryeong.mineme.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.najudoryeong.mineme.common_ui.BaseFragment
+import com.najudoryeong.mineme.home.databinding.FragmentHomeBinding
+
+
+class HomeFragment : BaseFragment<FragmentHomeBinding>(Home) {
+
+}
