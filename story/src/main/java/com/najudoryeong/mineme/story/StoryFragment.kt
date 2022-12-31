65f7ce514ea19f04a0f0d7b29dd4d36c5f5472e7
KDW03 <aszx0628@naver.com>
2022-12-31 18:53:10 +0900

    [MIN-22] feature : story 모듈 생성(#10)

---
+++ story/src/main/java/com/najudoryeong/mineme/story/StoryFragment.kt
@@ -0,0 +1,15 @@
+package com.najudoryeong.mineme.story
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.najudoryeong.mineme.common_ui.BaseFragment
+import com.najudoryeong.mineme.common_ui.MainViewModelUtil
+import com.najudoryeong.mineme.story.databinding.FragmentStoryBinding
+
+class StoryFragment : BaseFragment<FragmentStoryBinding>(Story){
+
+
+
+}
