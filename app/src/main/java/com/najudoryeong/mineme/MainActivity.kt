b5fc0b918d25b6c904f18db8f752f7e2ab55ce73
KDW03 <aszx0628@naver.com>
2022-12-31 18:54:09 +0900

    [MIN-22] feature : MainViewModel 프래그먼트 전환에 따른 툴바 텍스트 옵저빙 구현(#10)

--- app/src/main/java/com/najudoryeong/mineme/MainActivity.kt
+++ app/src/main/java/com/najudoryeong/mineme/MainActivity.kt
@@ -2,33 +2,56 @@ package com.najudoryeong.mineme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
+import androidx.databinding.Observable
+import androidx.lifecycle.Observer
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
+import com.najudoryeong.mineme.common_ui.MainViewModelUtil
 import com.najudoryeong.mineme.databinding.ActivityMainBinding
 
-
-class MainActivity : AppCompatActivity() {
+// 하위 모듈이 MainViewModel 코드에 접근할 수 있게 MainViewModelUtil 상속 구현
+class MainActivity : AppCompatActivity(), MainViewModelUtil {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private val model: MainViewModel by viewModels()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.viewModel = model
+        model.toolbarTitle.observe(this, toolbarTitleObserver)
+
         initAppBar()
         initBottomNav()
     }
 
     private fun initBottomNav() {
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_containerView) as NavHostFragment
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment_containerView) as NavHostFragment
         val navController = navHostFragment.navController
 
-        binding.bottomNavigationView.let {
-            it.setupWithNavController(navController)
-            it.itemIconTintList = null
-        }
+        // appbar 구성 요소 설정
+        // 바텀네비게이션과 연결하면 해당 프래그먼트에 네비게이션으로는 backButton x
+        appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.storyFragment)
+        )
 
+        // navController와 actionbar 연결
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
+
+        binding.bottomNavigationView.run {
+            setupWithNavController(navController)
+            itemIconTintList = null
+        }
     }
 
     private fun initAppBar() {
@@ -38,4 +61,16 @@ class MainActivity : AppCompatActivity() {
             setDisplayHomeAsUpEnabled(true)
         }
     }
+
+    /** [MainViewModelUtil] */
+    override fun setToolbarTitle(newTitle: String) {
+        model.updateToolbarTitle(newTitle)
+    }
+
+    //toolbar title 옵저버
+    private val toolbarTitleObserver = Observer<String> {
+        binding.toolbar.title = it
+    }
+
+
 }
