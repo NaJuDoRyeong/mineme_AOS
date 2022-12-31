1ca928a0573f5aa0e1dea787f965c3315e4df67c
KDW03 <aszx0628@naver.com>
2022-12-31 18:51:40 +0900

    [MIN-22] feature : common-ui 모듈 생성 및 BaseFragment 및 Util 구현 (#10)

---
+++ common-ui/src/main/java/com/najudoryeong/mineme/common_ui/MainViewModelUtil.kt
@@ -0,0 +1,8 @@
+package com.najudoryeong.mineme.common_ui
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+
+interface MainViewModelUtil{
+    fun setToolbarTitle(newTitle: String)
+}
