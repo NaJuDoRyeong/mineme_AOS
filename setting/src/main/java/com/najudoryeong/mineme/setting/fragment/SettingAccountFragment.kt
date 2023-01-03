36792b19491abdb5a28b5a92f5840eca6140c718
KDW03 <aszx0628@naver.com>
2023-01-03 20:26:11 +0900

    [MIN-25] feature : SettingAccount 화면 및 코드 복사 기능 구현(#13)

--- setting/src/main/java/com/najudoryeong/mineme/setting/fragment/SettingAccountFragment.kt
+++ setting/src/main/java/com/najudoryeong/mineme/setting/fragment/SettingAccountFragment.kt
@@ -1,17 +1,45 @@
 package com.najudoryeong.mineme.setting.fragment
 
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.Toast
 import com.najudoryeong.mineme.common_ui.BaseFragment
+import com.najudoryeong.mineme.setting.R
 import com.najudoryeong.mineme.setting.SettingAccount
 import com.najudoryeong.mineme.setting.databinding.FragmentSettingAccountBinding
 
 
-class SettingAccountFragment : BaseFragment<FragmentSettingAccountBinding>(SettingAccount) {
+class SettingAccountFragment : BaseFragment<FragmentSettingAccountBinding>(SettingAccount), OnClickListener {
 
     override fun initView() {
         binding.apply {
-
+            myCode.setOnClickListener(this@SettingAccountFragment)
+            targetCode.setOnClickListener(this@SettingAccountFragment)
         }
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.my_code -> {
+                val text = binding.myCode.text.toString()
+                clipText(text)
+            }
+            R.id.target_code -> {
+                val text = binding.targetCode.text.toString()
+                clipText(text)
+            }
+        }
+    }
+
+    fun clipText(text: String) {
+        val clipboard = activity?.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        val clip: ClipData = ClipData.newPlainText("AccountCode", text)
+        clipboard.setPrimaryClip(clip)
+        Toast.makeText(context, "클립보드에 복사되었습니다.", Toast.LENGTH_SHORT).show()
     }
 
 }
