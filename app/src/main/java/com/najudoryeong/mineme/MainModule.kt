0c1526ffd9f318efeba05f66991d0e61eb930ef4
KDW03 <aszx0628@naver.com>
2023-01-05 20:28:31 +0900

    [MIN-15] feature : 온보딩 모듈 생성 및 온보딩 화면 3개 구현(#16)

---
+++ app/src/main/java/com/najudoryeong/mineme/MainModule.kt
@@ -0,0 +1,15 @@
+package com.najudoryeong.mineme
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+class MainModule {
+    @Singleton
+    @Provides
+    fun provideMainActivityClass(): Class<*> = MainActivity::class.java
+}
--- home/src/main/java/com/najudoryeong/mineme/home/HomeFragment.kt
+++ home/src/main/java/com/najudoryeong/mineme/home/HomeFragment.kt
@@ -1,10 +1,5 @@
 package com.najudoryeong.mineme.home
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.najudoryeong.mineme.common_ui.BaseFragment
 import com.najudoryeong.mineme.home.databinding.FragmentHomeBinding
 
